bf18835342d80b5aa1560b349dd7bc5382134cce
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2023-09-08 17:07:38 +0200

    add PRE and BUILD parts for dev gems build

--- lib/datadog/ci/version.rb
+++ lib/datadog/ci/version.rb
@@ -6,8 +6,11 @@ module Datadog
       MAJOR = "0"
       MINOR = "1"
       PATCH = "0"
+      PRE = nil
+      BUILD = nil
+      # PRE and BUILD above are modified for dev gems during gem build GHA workflow
 
-      STRING = [MAJOR, MINOR, PATCH].compact.join(".")
+      STRING = [MAJOR, MINOR, PATCH, PRE, BUILD].compact.join(".")
 
       MINIMUM_RUBY_VERSION = "2.1.0"
 
