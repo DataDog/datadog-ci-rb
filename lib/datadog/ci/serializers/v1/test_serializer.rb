0eede7c2c4c512e356a576584be2758b53dcc92e
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2023-09-18 17:00:19 +0200

    trace serializers structure

---
+++ lib/datadog/ci/serializers/v1/test_serializer.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require "../base"
+
+module Datadog
+  module CI
+    module Serializers
+      module V1
+        class TestSerializer < Base
+        end
+      end
+    end
+  end
+end
