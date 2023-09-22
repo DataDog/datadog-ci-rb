64be17c538d885cf9015d2dbdc7652c858cf665e
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2023-09-22 13:18:56 +0200

    construct agentless transport in CI component

--- lib/datadog/ci/configuration/components.rb
+++ lib/datadog/ci/configuration/components.rb
@@ -16,6 +16,38 @@ module Datadog
         end
 
         def activate_ci!(settings)
+          agentless_transport = nil
+          writer_options = settings.ci.writer_options
+
+          if settings.ci.agentless_mode_enabled
+            if settings.api_key.nil?
+              # agentless mode is requested but no API key is provided -
+              # we cannot continue and log an error
+              # Tests are running without CI visibility enabled
+
+              Datadog.logger.error(
+                "Agentless mode was enabled but DD_API_KEY is not set: CI visibility is disabled. " \
+                "Please make sure to set valid api key in DD_API_KEY environment variable"
+              )
+
+              settings.ci.enabled = false
+              return
+            else
+              agentless_transport = Datadog::CI::TestVisibility::Transport.new(api_key: settings.api_key)
+            end
+          end
+
+          writer_options[:transport] = agentless_transport if agentless_transport
+
+          # Deactivate telemetry
+          settings.telemetry.enabled = false
+
+          # Deactivate remote configuration
+          settings.remote.enabled = false
+
+          # enable tracing
+          settings.tracing.enabled = true
+
           # Activate underlying tracing test mode
           settings.tracing.test_mode.enabled = true
 
