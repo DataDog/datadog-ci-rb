3ceada4be74437dd120905ec54d1d523560ad29f
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2023-11-01 10:37:53 +0100

    fix agent connection errors in specs

--- spec/support/ci_mode_helpers.rb
+++ spec/support/ci_mode_helpers.rb
@@ -3,6 +3,10 @@ RSpec.shared_context "CI mode activated" do
   let(:integration_options) { {} }
 
   before do
+    allow_any_instance_of(Datadog::Core::Remote::Negotiation).to(
+      receive(:endpoint?).with("/evp_proxy/v2/").and_return(true)
+    )
+
     Datadog.configure do |c|
       c.ci.enabled = true
       c.ci.instrument integration_name, integration_options
