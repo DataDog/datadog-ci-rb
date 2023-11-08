a91b81f14faa17a20c9e28905f5c75ba7c040f28
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2023-11-08 10:09:59 +0100

    public api documentation

--- lib/datadog/ci/span.rb
+++ lib/datadog/ci/span.rb
@@ -3,7 +3,6 @@
 require_relative "ext/test"
 
 module Datadog
-  # Public API for Datadog CI visibility
   module CI
     # Represents a single part of a test run.
     # Could be a session, suite, test, or any custom span.
@@ -16,46 +15,73 @@ module Datadog
         @tracer_span = tracer_span
       end
 
+      # @return [String] the name of the span.
       def name
         tracer_span.name
       end
 
+      # @return [String] the type of the span (for example "test" or type that was provided to [Datadog::CI.trace]).
+      def span_type
+        tracer_span.type
+      end
+
+      # Sets the status of the span to "pass".
+      # @return [void]
       def passed!
         tracer_span.set_tag(Ext::Test::TAG_STATUS, Ext::Test::Status::PASS)
       end
 
+      # Sets the status of the span to "fail".
+      # @param [Exception] exception the exception that caused the test to fail.
+      # @return [void]
       def failed!(exception: nil)
         tracer_span.status = 1
         tracer_span.set_tag(Ext::Test::TAG_STATUS, Ext::Test::Status::FAIL)
         tracer_span.set_error(exception) unless exception.nil?
       end
 
+      # Sets the status of the span to "skip".
+      # @param [Exception] exception the exception that caused the test to fail.
+      # @param [String] reason the reason why the test was skipped.
+      # @return [void]
       def skipped!(exception: nil, reason: nil)
         tracer_span.set_tag(Ext::Test::TAG_STATUS, Ext::Test::Status::SKIP)
         tracer_span.set_error(exception) unless exception.nil?
         tracer_span.set_tag(Ext::Test::TAG_SKIP_REASON, reason) unless reason.nil?
       end
 
+      # Gets tag value by key.
+      # @param [String] key the key of the tag.
+      # @return [String] the value of the tag.
       def get_tag(key)
         tracer_span.get_tag(key)
       end
 
+      # Sets tag value by key.
+      # @param [String] key the key of the tag.
+      # @param [String] value the value of the tag.
+      # @return [void]
       def set_tag(key, value)
         tracer_span.set_tag(key, value)
       end
 
+      # Sets metric value by key.
+      # @param [String] key the key of the metric.
+      # @param [Numeric] value the value of the metric.
+      # @return [void]
       def set_metric(key, value)
         tracer_span.set_metric(key, value)
       end
 
+      # Finishes the span.
+      # @return [void]
       def finish
         tracer_span.finish
       end
 
-      def span_type
-        tracer_span.type
-      end
-
+      # Sets multiple tags at once.
+      # @param [Hash[String, String]] tags the tags to set.
+      # @return [void]
       def set_tags(tags)
         tags.each do |key, value|
           tracer_span.set_tag(key, value)
