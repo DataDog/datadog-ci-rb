b77fe2ce64dc04f2a860edcf9fc7e86cce6dfdf9
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2023-12-12 13:14:23 +0100

    additional debug logging to investigate issues with test suite level visibility

--- spec/datadog/ci/test_spec.rb
+++ spec/datadog/ci/test_spec.rb
@@ -22,4 +22,54 @@ RSpec.describe Datadog::CI::Test do
       expect(Datadog::CI).to have_received(:deactivate_test).with(ci_test)
     end
   end
+
+  describe "#test_suite_id" do
+    subject(:test_suite_id) { ci_test.test_suite_id }
+    let(:ci_test) { described_class.new(tracer_span) }
+
+    before do
+      allow(tracer_span).to receive(:get_tag).with(Datadog::CI::Ext::Test::TAG_TEST_SUITE_ID).and_return("test suite id")
+    end
+
+    it { is_expected.to eq("test suite id") }
+  end
+
+  describe "#test_suite_name" do
+    subject(:test_suite_name) { ci_test.test_suite_name }
+    let(:ci_test) { described_class.new(tracer_span) }
+
+    before do
+      allow(tracer_span).to(
+        receive(:get_tag).with(Datadog::CI::Ext::Test::TAG_SUITE).and_return("test suite name")
+      )
+    end
+
+    it { is_expected.to eq("test suite name") }
+  end
+
+  describe "#test_module_id" do
+    subject(:test_module_id) { ci_test.test_module_id }
+    let(:ci_test) { described_class.new(tracer_span) }
+
+    before do
+      allow(tracer_span).to(
+        receive(:get_tag).with(Datadog::CI::Ext::Test::TAG_TEST_MODULE_ID).and_return("test module id")
+      )
+    end
+
+    it { is_expected.to eq("test module id") }
+  end
+
+  describe "#test_session_id" do
+    subject(:test_session_id) { ci_test.test_session_id }
+    let(:ci_test) { described_class.new(tracer_span) }
+
+    before do
+      allow(tracer_span).to(
+        receive(:get_tag).with(Datadog::CI::Ext::Test::TAG_TEST_SESSION_ID).and_return("test session id")
+      )
+    end
+
+    it { is_expected.to eq("test session id") }
+  end
 end
