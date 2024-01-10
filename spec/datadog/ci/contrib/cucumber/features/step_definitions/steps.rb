46ce9176fdc83c75dd523e806696430eab628f38
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2024-01-10 15:24:24 +0100

    add cucumber feature with examples to instrumentation specs

--- spec/datadog/ci/contrib/cucumber/features/step_definitions/steps.rb
+++ spec/datadog/ci/contrib/cucumber/features/step_definitions/steps.rb
@@ -5,3 +5,11 @@ end
 Then "failure" do
   expect(1 + 1).to eq(3)
 end
+
+Then(/I add (-?\d+) and (-?\d+)/) do |n1, n2|
+  @res = n1.to_i + n2.to_i
+end
+
+Then(/the result should be (-?\d+)/) do |res|
+  expect(@res).to eq(res.to_i)
+end
