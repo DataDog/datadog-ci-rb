c8e8d9310ddd2a0d3eaa3fb84a6f2c25d0a7c2a6
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2024-03-15 11:01:52 +0100

    add Datadog::CI::Cov placeholder

---
+++ lib/datadog/ci/cov.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Datadog
+  module CI
+    # Placeholder for code coverage collection
+    # Implementation in ext/datadog_cov
+    class Cov
+    end
+  end
+end
