4cd18d85936df15d8cef1c77f9b3d4bdaa1d6ae6
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2024-03-25 13:55:14 +0100

    Bump version 1.0.0.beta1

--- lib/datadog/ci/version.rb
+++ lib/datadog/ci/version.rb
@@ -3,10 +3,10 @@
 module Datadog
   module CI
     module VERSION
-      MAJOR = "0"
-      MINOR = "8"
-      PATCH = "3"
-      PRE = nil
+      MAJOR = "1"
+      MINOR = "0"
+      PATCH = "0"
+      PRE = "beta1"
       BUILD = nil
       # PRE and BUILD above are modified for dev gems during gem build GHA workflow
 
