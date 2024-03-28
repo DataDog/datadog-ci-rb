7eaef53c74c9c93176b35d695b385f4517303368
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2024-03-28 12:14:12 +0100

    more thorough testing of Coverage::Writer

--- spec/datadog/ci/itr/coverage/writer_spec.rb
+++ spec/datadog/ci/itr/coverage/writer_spec.rb
@@ -169,4 +169,149 @@ RSpec.describe Datadog::CI::ITR::Coverage::Writer do
         .to(true)
     end
   end
+
+  describe "#stop" do
+    before { skip if PlatformHelpers.jruby? }
+
+    subject(:stop) { writer.stop }
+
+    shared_context "shuts down the worker" do
+      before do
+        expect(writer.buffer).to receive(:close).at_least(:once)
+
+        # Do this to prevent cleanup from breaking the test
+        allow(writer).to receive(:join)
+          .with(0)
+          .and_return(true)
+
+        allow(writer).to receive(:join)
+          .with(described_class::DEFAULT_SHUTDOWN_TIMEOUT)
+          .and_return(true)
+      end
+    end
+
+    context "when the worker has not been started" do
+      before do
+        expect(writer.buffer).to_not receive(:close)
+        allow(writer).to receive(:join)
+          .with(described_class::DEFAULT_SHUTDOWN_TIMEOUT)
+          .and_return(true)
+      end
+
+      it { is_expected.to be false }
+    end
+
+    context "when the worker has been started" do
+      include_context "shuts down the worker"
+
+      before do
+        writer.perform
+        try_wait_until { writer.running? && writer.run_loop? }
+      end
+
+      it { is_expected.to be true }
+    end
+
+    context "called multiple times with graceful stop" do
+      include_context "shuts down the worker"
+
+      before do
+        writer.perform
+        try_wait_until { writer.running? && writer.run_loop? }
+      end
+
+      it do
+        expect(writer.stop).to be true
+        try_wait_until { !writer.running? }
+        expect(writer.stop).to be false
+      end
+    end
+
+    context "given force_stop: true" do
+      subject(:stop) { writer.stop(true) }
+
+      context "and the worker does not gracefully stop" do
+        before do
+          # Make it ignore graceful stops
+          expect(writer.buffer).to receive(:close)
+          allow(writer).to receive(:stop_loop).and_return(false)
+          allow(writer).to receive(:join).and_return(nil)
+        end
+
+        context "after the worker has been started" do
+          before { writer.perform }
+
+          it do
+            is_expected.to be true
+
+            # Give thread time to be terminated
+            try_wait_until { !writer.running? }
+
+            expect(writer.run_async?).to be false
+            expect(writer.running?).to be false
+          end
+        end
+      end
+    end
+
+    context "given shutdown_timeout" do
+      let(:options) { {shutdown_timeout: 1000} }
+      include_context "shuts down the worker"
+
+      context "and the worker has been started" do
+        before do
+          expect(writer).to receive(:join).with(1000).and_return(true)
+
+          writer.perform
+          try_wait_until { writer.running? && writer.run_loop? }
+        end
+
+        it { is_expected.to be true }
+      end
+    end
+  end
+  describe "integration tests" do
+    describe "forking" do
+      before { skip "Fork not supported on current platform" unless Process.respond_to?(:fork) }
+      let(:flushed_events) { [] }
+
+      context "when the process forks and an event is written" do
+        let(:events) { [double("event"), double("event2")] }
+
+        before do
+          allow(writer).to receive(:after_fork)
+            .and_call_original
+          allow(writer.transport).to receive(:send_events)
+            .and_return([])
+        end
+
+        after { expect(writer.stop).to be_truthy }
+
+        it "does not drop any events" do
+          # Start writer in main process
+          writer.perform
+
+          expect_in_fork do
+            # Queue up events, wait for worker to process them.
+            events.each { |event| writer.write(event) }
+            try_wait_until(seconds: 3) { !writer.work_pending? }
+            writer.stop
+
+            # Verify state of the writer
+            expect(writer).to have_received(:after_fork).once
+            expect(writer.buffer).to be_empty
+            expect(writer.error?).to be false
+
+            expect(writer.transport).to have_received(:send_events).at_most(events.length).times do |events|
+              flushed_events.concat(events)
+            end
+
+            expect(events).to_not be_empty
+            expect(events).to have(events.length).items
+            expect(events).to include(*events)
+          end
+        end
+      end
+    end
+  end
 end
---
+++ spec/support/synchronization_helpers.rb
@@ -0,0 +1,103 @@
+# frozen_string_literal: true
+
+module SynchronizationHelpers
+  def expect_in_fork(fork_expectations: nil, timeout_seconds: 10)
+    fork_expectations ||= proc { |status:, stdout:, stderr:|
+      expect(status && status.success?).to be(true), "STDOUT:`#{stdout}` STDERR:`#{stderr}"
+    }
+
+    fork_stdout = Tempfile.new("ddtrace-rspec-expect-in-fork-stdout")
+    fork_stderr = Tempfile.new("ddtrace-rspec-expect-in-fork-stderr")
+    begin
+      # Start in fork
+      pid = fork do
+        # Capture forked output
+        $stdout.reopen(fork_stdout)
+        $stderr.reopen(fork_stderr) # STDERR captures RSpec failures. We print it in case the fork fails on exit.
+
+        yield
+      end
+
+      fork_stderr.close
+      fork_stdout.close
+
+      # Wait for fork to finish, retrieve its status.
+      # Enforce timeout to ensure test fork doesn't hang the test suite.
+      _, status = try_wait_until(seconds: timeout_seconds) { Process.wait2(pid, Process::WNOHANG) }
+
+      stdout = File.read(fork_stdout.path)
+      stderr = File.read(fork_stderr.path)
+
+      # Capture forked execution information
+      result = {status: status, stdout: stdout, stderr: stderr}
+
+      # Expect fork and assertions to have completed successfully.
+      fork_expectations.call(**result)
+
+      result
+    rescue => e
+      stdout ||= File.read(fork_stdout.path)
+      stderr ||= File.read(fork_stderr.path)
+
+      puts stdout
+      warn stderr
+
+      raise e
+    ensure
+      begin
+        Process.kill("KILL", pid)
+      rescue
+        nil
+      end # Prevent zombie processes on failure
+
+      fork_stdout.unlink
+      fork_stderr.unlink
+    end
+  end
+
+  # Waits for the condition provided by the block argument to return truthy.
+  #
+  # Waits for 5 seconds by default.
+  #
+  # Can be configured by setting either:
+  #   * `seconds`, or
+  #   * `attempts` and `backoff`
+  #
+  # @yieldreturn [Boolean] block executed until it returns truthy
+  # @param [Numeric] seconds number of seconds to wait
+  # @param [Integer] attempts number of attempts at checking the condition
+  # @param [Numeric] backoff wait time between condition checking attempts
+  def try_wait_until(seconds: nil, attempts: nil, backoff: nil)
+    raise "Provider either `seconds` or `attempts` & `backoff`, not both" if seconds && (attempts || backoff)
+
+    if seconds
+      attempts = seconds * 10
+      backoff = 0.1
+    else
+      # 5 seconds by default, but respect the provide values if any.
+      attempts ||= 50
+      backoff ||= 0.1
+    end
+
+    # It's common for tests to want to run simple tasks in a background thread
+    # but call this method without the thread having even time to start.
+    #
+    # We add an extra attempt, interleaved by `Thread.pass`, in order to allow for
+    # those simple cases to quickly succeed without a timed `sleep` call. This will
+    # save simple test one `backoff` seconds sleep cycle.
+    #
+    # The total configured timeout is not reduced.
+    (attempts + 1).times do |i|
+      result = yield(attempts)
+      return result if result
+
+      if i == 0
+        Thread.pass
+      else
+        sleep(backoff)
+      end
+    end
+
+    raise("Wait time exhausted!")
+  end
+end
