03385faf70c1adf7a10ae4910d3935d8e4765fc7
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2024-03-28 13:58:40 +0100

    add CoverageHelpers module

--- lib/datadog/ci/itr/runner.rb
+++ lib/datadog/ci/itr/runner.rb
@@ -88,6 +88,7 @@ module Datadog
 
           Datadog.logger.debug { "Writing coverage event \n #{event.pretty_inspect}" }
 
+          p "calling write"
           write(event)
 
           event
@@ -100,6 +101,7 @@ module Datadog
         private
 
         def write(event)
+          p "here I am in write"
           # skip sending events if writer is not configured
           @coverage_writer&.write(event)
         end
