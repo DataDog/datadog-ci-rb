2e0386497bad18ace7d2d68a2740106e35541c10
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2024-04-09 11:57:12 +0200

    add Packfiles module that encapsulates generating git packfiles in some folder and yields them one y one

--- lib/datadog/ci/git/tree_uploader.rb
+++ lib/datadog/ci/git/tree_uploader.rb
@@ -6,6 +6,7 @@ require "fileutils"
 require_relative "local_repository"
 require_relative "search_commits"
 require_relative "upload_packfile"
+require_relative "packfiles"
 
 module Datadog
   module CI
@@ -23,7 +24,7 @@ module Datadog
           # TO BE ADDED IN CIVIS-2863
           # 3. Get a maximum of 1000 latest commits in the last month with git log
           latest_commits = LocalRepository.git_commits
-          head_commit = latest_commits.first
+          head_commit = latest_commits&.first
           if head_commit.nil?
             Datadog.logger.debug("Got empty latest commits list, aborting git upload")
             return
@@ -55,52 +56,11 @@ module Datadog
             repository_url: repository_url
           )
 
-          # TODO: extract packfile generation to a separate class that yields files one by one
-          # hopefully it'll make it easier to test
-          Dir.mktmpdir do |tmpdir|
-            packfiles_folder = tmpdir
-            prefix = LocalRepository.git_generate_packfiles(
-              included_commits: included_commits,
-              excluded_commits: backend_commits.to_a,
-              path: packfiles_folder
-            )
-
-            if prefix.nil?
-              Datadog.logger.debug("packfiles generation failed, retrying with different folder")
-
-              packfiles_folder = File.join(Dir.pwd, "tmp", "packfiles")
-              FileUtils.mkdir_p(packfiles_folder)
-
-              prefix = LocalRepository.git_generate_packfiles(
-                included_commits: included_commits,
-                excluded_commits: backend_commits.to_a,
-                path: packfiles_folder
-              )
-
-              if prefix.nil?
-                Datadog.logger.debug("packfiles generation failed, aborting git upload")
-                break
-              end
-            end
-
-            packfiles = Dir.entries(packfiles_folder) - %w[. ..]
-            if packfiles.empty?
-              Datadog.logger.debug("Empty packfiles list, nothing to upload")
-              break
-            end
-
-            # 7. Upload packfiles via an HTTP multipart POST to /api/v2/git/repository/packfile for each generated packfile
-            packfiles.each do |packfile|
-              filename = File.basename(packfile)
-
-              next unless filename.start_with?(prefix)
-              next unless filename.end_with?(".pack")
-
-              uploader.call(filepath: packfile)
-            rescue UploadPackfile::ApiError => e
-              Datadog.logger.debug("Packfile upload failed with #{e}")
-              break
-            end
+          Packfiles.generate(included_commits: included_commits, excluded_commits: backend_commits) do |filepath|
+            uploader.call(filepath: filepath)
+          rescue UploadPackfile::ApiError => e
+            Datadog.logger.debug("Packfile upload failed with #{e}")
+            break
           end
         end
       end
