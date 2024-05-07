264765ced6697a8d11582032f092bbcd2db4a69a
Andrey <andrey.marchenko@datadoghq.com>
2024-05-07 14:50:33 +0200

    instrument knapsack_pro runner in queue mode

---
+++ lib/datadog/ci/contrib/rspec/knapsack_pro/extension.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "knapsack_pro/extensions/rspec_extension"
+
+require_relative "runner"
+
+module Datadog
+  module CI
+    module Contrib
+      module RSpec
+        # Instrument RSpec::Core::Example
+        module KnapsackPro
+          module Extension
+            def self.included(base)
+              base.singleton_class.prepend(ClassMethods)
+            end
+
+            module ClassMethods
+              def setup!
+                super
+
+                ::RSpec::Core::Runner.include(Datadog::CI::Contrib::RSpec::KnapsackPro::Runner)
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
--- lib/datadog/ci/contrib/rspec/patcher.rb
+++ lib/datadog/ci/contrib/rspec/patcher.rb
@@ -27,6 +27,13 @@ module Datadog
               ::RSpec::Queue::Runner.include(Runner)
             end
 
+            # Knapsack Pro test runner instrumentation
+            # https://github.com/KnapsackPro/knapsack_pro-ruby
+            if knapsack_pro?
+              require_relative "knapsack_pro/extension"
+              ::KnapsackPro::Extensions::RSpecExtension.include(KnapsackPro::Extension)
+            end
+
             ::RSpec::Core::Runner.include(Runner)
             ::RSpec::Core::Example.include(Example)
             ::RSpec::Core::ExampleGroup.include(ExampleGroup)
@@ -34,6 +41,14 @@ module Datadog
 
           def ci_queue?
             defined?(::RSpec::Queue::Runner)
+          end
+
+          def knapsack_pro?
+            knapsack_version = Gem.loaded_specs["knapsack_pro"]&.version
+
+            # additional instrumentation is needed for KnapsackPro version 7 and later
+            defined?(::KnapsackPro) &&
+              knapsack_version && knapsack_version >= Gem::Version.new("7")
           end
         end
       end
