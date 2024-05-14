b2ce1b38c808901864f3f424829c670c69740290
Andrey <andrey.marchenko@datadoghq.com>
2024-05-14 10:48:35 +0200

    Bump version 1.0.0.beta3 to 1.0.0.beta4

--- lib/datadog/ci/version.rb
+++ lib/datadog/ci/version.rb
@@ -6,7 +6,7 @@ module Datadog
       MAJOR = "1"
       MINOR = "0"
       PATCH = "0"
-      PRE = "beta3"
+      PRE = 'beta4'
       BUILD = nil
       # PRE and BUILD above are modified for dev gems during gem build GHA workflow
 
