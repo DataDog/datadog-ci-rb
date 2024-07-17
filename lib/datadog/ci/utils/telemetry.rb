eed3e38520b51b65f1c37dc3ff466537beaca45c
Andrey <andrey.marchenko@datadoghq.com>
2024-07-17 12:38:01 +0200

    add telemetry constants, helpers, signatures

---
+++ lib/datadog/ci/utils/telemetry.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require_relative "../ext/telemetry"
+
+module Datadog
+  module CI
+    module Utils
+      module Telemetry
+        def self.inc(metric_name, count, tags = {})
+          telemetry.inc(Ext::Telemetry::NAMESPACE, metric_name, count, tags: tags)
+        end
+
+        def self.distribution(metric_name, value, tags = {})
+          telemetry.distribution(Ext::Telemetry::NAMESPACE, metric_name, value, tags: tags)
+        end
+
+        def self.telemetry
+          Datadog.send(:components).telemtry
+        end
+      end
+    end
+  end
+end
