4d1619cbba04844e80e385f9d35067d9a7a7eac1
Andrey <andrey.marchenko@datadoghq.com>
2024-11-26 10:50:42 +0100

    if auto instrumentation is used in JRuby, print error and bail out

--- lib/datadog/ci/auto_instrument.rb
+++ lib/datadog/ci/auto_instrument.rb
@@ -1,3 +1,8 @@
 require "datadog/ci"
 
+if RUBY_ENGINE == "jruby"
+  Datadog.logger.error("Auto instrumentation is not supported on JRuby. Please use manual instrumentation instead.")
+  return
+end
+
 Datadog::CI::Contrib::Instrumentation.auto_instrument
