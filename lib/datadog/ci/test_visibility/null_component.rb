2a6a04b01c89c38f550b7fc2b632024ffd8bca11
Andrey <andrey.marchenko@datadoghq.com>
2025-02-14 15:10:29 +0100

    fix some test failures

--- lib/datadog/ci/test_visibility/null_component.rb
+++ lib/datadog/ci/test_visibility/null_component.rb
@@ -5,6 +5,9 @@ module Datadog
     module TestVisibility
       # Special test visibility component that does not record anything
       class NullComponent
+        def configure(_, _)
+        end
+
         def start_test_session(service: nil, tags: {}, total_tests_count: 0)
           skip_tracing
         end
