1aed7592362fa00687ad215beadfb0aa648db2f9
Andrey <andrey.marchenko@datadoghq.com>
2025-02-17 13:19:13 +0100

    skeleton of TestManagement::Component

---
+++ lib/datadog/ci/test_management/component.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Datadog
+  module CI
+    module TestManagement
+      # Test management is a feature that lets people manage their flaky tests in Datadog.
+      # It includes:
+      # - marking test as quarantined causes test to continue running but not failing the build
+      # - marking test as disabled causes test to be skipped
+      # - marking test as "attempted to fix" causes test to be retried many times to confirm that fix worked
+      class Component
+        def initialize(
+          enabled:,
+          attempt_to_fix_retries_count:
+        )
+          @enabled = enabled
+          @attempt_to_fix_retries_count = attempt_to_fix_retries_count
+        end
+
+        def configure(library_settings, test_session)
+        end
+      end
+    end
+  end
+end
