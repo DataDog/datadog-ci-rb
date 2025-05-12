701c0686dc47431eedd428996f6acff83c6159d8
Andrey <andrey.marchenko@datadoghq.com>
2025-05-12 13:01:17 +0200

    flaky test management: add sha parameter to the tests_properties request

--- lib/datadog/ci/test_management/tests_properties.rb
+++ lib/datadog/ci/test_management/tests_properties.rb
@@ -118,7 +118,8 @@ module Datadog
               "type" => Ext::Transport::DD_API_TEST_MANAGEMENT_TESTS_TYPE,
               "attributes" => {
                 "repository_url" => test_session.git_repository_url,
-                "commit_message" => test_session.git_commit_message
+                "commit_message" => test_session.git_commit_message,
+                "sha" => test_session.git_commit_sha
               }
             }
           }.to_json
