1fc7e4579bf4e3403d3c724e04b622f3c1d402e0
Andrey <andrey.marchenko@datadoghq.com>
2025-11-17 14:07:11 +0100

    rename wrong test.retry_reason value for Auto Test Retries feature

--- lib/datadog/ci/ext/test.rb
+++ lib/datadog/ci/ext/test.rb
@@ -156,7 +156,7 @@ module Datadog
         # possible reasons why a test was retried
         module RetryReason
           RETRY_DETECT_FLAKY = "early_flake_detection"
-          RETRY_FAILED = "auto_test_retries"
+          RETRY_FAILED = "auto_test_retry"
           RETRY_FLAKY_FIXED = "attempt_to_fix"
           RETRY_EXTERNAL = "external"
 
